b02d696c44f6472f462c4e3513f713e48c85e43e
Lovina Bhat <lovinabhatia06@gmail.com>
2026-03-21 01:25:14 +0530

    first commit

---
+++ src/Components/Button.jsx
@@ -0,0 +1,18 @@
+const Button = ({ id, title, leftIcon, rightIcon, containerClass }) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden
+        rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        
+        {leftIcon}
+        {title}
+        {rightIcon}
+      </span>
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/Components/tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],  
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "em-monde": ["em-monde", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
