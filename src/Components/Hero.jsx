a2dd9851da95e18d3d889242e132886636e47563
Lovina Bhat <lovinabhatia06@gmail.com>
2026-03-26 12:18:04 +0530

    Add trailer modal and updated Hero component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -131,6 +131,7 @@ const Hero = () => {
                 src={getVideoSrc(upcomingVideoIndex)}
                 loop
                 muted
+                playsInline
                 id="current-video"
                 className="size-64 origin-center scale-150 object-cover object-center"
                 onLoadedData={handleVideoLoad}
@@ -143,6 +144,7 @@ const Hero = () => {
             src={getVideoSrc(currentIndex)}
             loop
             muted
+            playsInline
             id="next-video"
             className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
             onLoadedData={handleVideoLoad}
@@ -153,6 +155,7 @@ const Hero = () => {
             autoPlay
             loop
             muted
+            playsInline
             className="absolute left-0 top-0 size-full object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
